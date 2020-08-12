68c620fe2095e70e5248f136d9d699ffcf1b87ab
Steve Ryan <syntithenai@gmail.com>
2020-08-12 10:42:46 +1000

    Updates

---
+++ precache-manifest.ae412a9fdce1829a3fb78d6eb06f5588.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3568f4225d3c7e4ef4ce964998e73496",
+    "url": "/opennludata/index.html"
+  },
+  {
+    "revision": "4fb68fdba72acccd5128",
+    "url": "/opennludata/static/css/2.690df615.chunk.css"
+  },
+  {
+    "revision": "632b46ed08ebc7765ad8",
+    "url": "/opennludata/static/css/main.8b532490.chunk.css"
+  },
+  {
+    "revision": "4fb68fdba72acccd5128",
+    "url": "/opennludata/static/js/2.dd0977fc.chunk.js"
+  },
+  {
+    "revision": "3b99ef8cf73f2faa2eae50c0c6928584",
+    "url": "/opennludata/static/js/2.dd0977fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "632b46ed08ebc7765ad8",
+    "url": "/opennludata/static/js/main.37ac5555.chunk.js"
+  },
+  {
+    "revision": "4f0b5ac349115a8a37e4",
+    "url": "/opennludata/static/js/runtime-main.e956959c.js"
+  },
+  {
+    "revision": "2b9b4872cd25494093c1eb14f0264a0b",
+    "url": "/opennludata/static/media/jsoneditor-icons.2b9b4872.svg"
+  }
+]);
