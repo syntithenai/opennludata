334e73f9313010846bc8117fcc8cfa888f82e313
Steve Ryan <syntithenai@gmail.com>
2020-08-12 16:40:24 +1000

    Updates

---
+++ precache-manifest.ea1afaf86199552aafb80d9ffc68047a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d562f9c56dbf1170c1f24a4642c0bb24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c64be85c407a65141fc",
+    "url": "/static/css/2.d715a5a4.chunk.css"
+  },
+  {
+    "revision": "6b669434af229eadf6d9",
+    "url": "/static/css/main.8b532490.chunk.css"
+  },
+  {
+    "revision": "0c64be85c407a65141fc",
+    "url": "/static/js/2.27cccf05.chunk.js"
+  },
+  {
+    "revision": "3b99ef8cf73f2faa2eae50c0c6928584",
+    "url": "/static/js/2.27cccf05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b669434af229eadf6d9",
+    "url": "/static/js/main.c5252227.chunk.js"
+  },
+  {
+    "revision": "8b9346fb80b0363fe21e",
+    "url": "/static/js/runtime-main.25976209.js"
+  },
+  {
+    "revision": "f29ee7056fbf2311979a414a4daa74b7",
+    "url": "/static/media/arrow-thick-left.f29ee705.svg"
+  },
+  {
+    "revision": "9ba7683e1279f7e90f69971e737b1b10",
+    "url": "/static/media/arrow-thick-right.9ba7683e.svg"
+  },
+  {
+    "revision": "8ab01bafd2d50d3b253c50114cd8d49b",
+    "url": "/static/media/check.8ab01baf.svg"
+  },
+  {
+    "revision": "ecd49b13b4d2c8548dd45d269b575a9d",
+    "url": "/static/media/fire.ecd49b13.svg"
+  },
+  {
+    "revision": "2b9b4872cd25494093c1eb14f0264a0b",
+    "url": "/static/media/jsoneditor-icons.2b9b4872.svg"
+  },
+  {
+    "revision": "73b0ce24fa1acaf0b9e382604eabb4ab",
+    "url": "/static/media/thumb-down.73b0ce24.svg"
+  },
+  {
+    "revision": "f18de18339339d464a5c0eefe0afb197",
+    "url": "/static/media/thumb-up.f18de183.svg"
+  },
+  {
+    "revision": "d362cb3f61deb313ac87505e59641c7b",
+    "url": "/static/media/waiting.d362cb3f.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/opennludata/precache-manifest.ae412a9fdce1829a3fb78d6eb06f5588.js"
+  "/precache-manifest.ea1afaf86199552aafb80d9ffc68047a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/opennludata/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
