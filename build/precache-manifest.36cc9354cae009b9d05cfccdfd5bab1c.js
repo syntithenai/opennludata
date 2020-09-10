cec1a5dfafc9f1255ebd1617269b11d60b6c8bd1
Steve Ryan <syntithenai@gmail.com>
2020-09-10 11:14:58 +1000

    add build

---
+++ build/precache-manifest.36cc9354cae009b9d05cfccdfd5bab1c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2498d1f1ead4b9ffc2ba41a111f54ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b5c476e067076e02077",
+    "url": "/static/css/2.6a953f83.chunk.css"
+  },
+  {
+    "revision": "0f477501c96e078f5fd2",
+    "url": "/static/css/main.4bf7a670.chunk.css"
+  },
+  {
+    "revision": "6b5c476e067076e02077",
+    "url": "/static/js/2.b27cf349.chunk.js"
+  },
+  {
+    "revision": "0f477501c96e078f5fd2",
+    "url": "/static/js/main.5a22f884.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f29ee7056fbf2311979a414a4daa74b7",
+    "url": "/static/media/arrow-thick-left.f29ee705.svg"
+  },
+  {
+    "revision": "9ba7683e1279f7e90f69971e737b1b10",
+    "url": "/static/media/arrow-thick-right.9ba7683e.svg"
+  },
+  {
+    "revision": "8ab01bafd2d50d3b253c50114cd8d49b",
+    "url": "/static/media/check.8ab01baf.svg"
+  },
+  {
+    "revision": "646ef0b80bc49b87150029d50713dbb6",
+    "url": "/static/media/helpoverview.646ef0b8.svg"
+  },
+  {
+    "revision": "2b9b4872cd25494093c1eb14f0264a0b",
+    "url": "/static/media/jsoneditor-icons.2b9b4872.svg"
+  },
+  {
+    "revision": "5d08cb4f7b6f6f2105f17a560a9c02c1",
+    "url": "/static/media/singingman.5d08cb4f.svg"
+  },
+  {
+    "revision": "73b0ce24fa1acaf0b9e382604eabb4ab",
+    "url": "/static/media/thumb-down.73b0ce24.svg"
+  },
+  {
+    "revision": "f18de18339339d464a5c0eefe0afb197",
+    "url": "/static/media/thumb-up.f18de183.svg"
+  },
+  {
+    "revision": "d362cb3f61deb313ac87505e59641c7b",
+    "url": "/static/media/waiting.d362cb3f.gif"
+  }
+]);
