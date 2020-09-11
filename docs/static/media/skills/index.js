40ce769ca565e47950fe78956e96d9e6ba6bc63f
Steve Ryan <syntithenai@gmail.com>
2020-09-12 09:09:52 +1000

    Published skill mym (5f5966485550fc1996ca8077) by Syntithenai

--- docs/static/media/skills/index.js
+++ docs/static/media/skills/index.js
@@ -1,1 +1,1 @@
-{"5f5b141cdaa5860b52c408a1":{"id":"5f5b141cdaa5860b52c408a1","entities":0,"intents":0,"utterances":0,"regexps":0,"file":"undefined-5f5b141cdaa5860b52c408a1.json"}}
+{"5f5966485550fc1996ca8077":{"id":"5f5966485550fc1996ca8077","_id":"5f5c03be64f324141661c57b","title":"mym","userAvatar":"Syntithenai","updated_date":1599865790199,"created_date":1599865790199,"tags":[],"entities":7,"intents":18,"utterances":0,"regexps":9,"file":"Syntithenai-mym-5f5966485550fc1996ca8077.json"}}
