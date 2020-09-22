a2c9b5c7a777afc2f7d0c25acd69136d1e4f9f58
Steve Ryan <syntithenai@gmail.com>
2020-09-23 06:07:26 +1000

    upgrade components, bump version

--- build_loginsystem.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone https://github.com/syntithenai/react-express-oauth-login-system.git
-cp .env ./react-express-oauth-login-system/
-cd ./react-express-oauth-login-system
-npm i
-npm run build
