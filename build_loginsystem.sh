f76eed3a55ad15397a5c123f8babc6a2158f1162
Steve Ryan <syntithenai@gmail.com>
2020-09-22 10:24:59 +1000

    wip

--- build_loginsystem.sh
+++ build_loginsystem.sh
@@ -2,4 +2,5 @@
 git clone https://github.com/syntithenai/react-express-oauth-login-system.git
 cp .env ./react-express-oauth-login-system/
 cd ./react-express-oauth-login-system
+npm i
 npm run build
