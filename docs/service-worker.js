7affb4f2b86e8e858530e55789d0c7cee521a4cb
Steve Ryan <syntithenai@gmail.com>
2020-11-03 14:03:58 +1100

    update website

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7cffb255c372c50174474dcdddeb9b41.js"
+  "/precache-manifest.831abb798cc3b184ed3c6932901f3211.js"
 );
 
 self.addEventListener('message', (event) => {
