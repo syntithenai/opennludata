9da08f9325db923ecad1a5309b37afc2299da993
Steve Ryan <syntithenai@gmail.com>
2020-11-09 10:56:19 +1100

    wip

--- src/api/index.js
+++ src/api/index.js
@@ -136,7 +136,9 @@ function startWebSocketAsr(server) {
         connection.on('close', function(reasonCode, description) {
             console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
         });
-        
+        connection.on('error', function(reasonCode, description) {
+            console.log([(new Date()) + ' Peer ' + connection.remoteAddress + ' error.',description]);
+        });
     });
 
 }
